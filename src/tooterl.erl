a745060d74cb662a0ec056bd91a15e75fa256ecb
Ian Brown <igb@hccp.org>
2022-12-04 15:43:21 -0800

    initital multipart attempt

--- src/tooterl.erl
+++ src/tooterl.erl
@@ -1,44 +1,68 @@
 -module(tooterl).
 -export([toot/3]).
 
+
+get_headers(AuthToken, ContentLength, ContentType) ->
+    [
+     {"Authorization", string:concat("Bearer ", AuthToken)}, 
+     {"Accept", "application/activity+json"},
+     {"User-Agent", "TootErl"},
+     {"Content-Length", integer_to_list(ContentLength)},
+     {"Content-Type",  ContentType}].
+
+
 toot(Status, AuthToken, Url)->
 
     Body=string:concat("status=", Status),
     
-    Headers = [
-	       {"Authorization", string:concat("Bearer ", AuthToken)}, 
-	       {"Accept", "application/activity+json"},
-	       {"User-Agent", "TootErl"},
-	       {"Content-Length", integer_to_list(length(Body))},
-	       {"Content-Type",  "application/x-www-form-urlencoded"}],
+    Headers = get_headers(AuthToken, length(Body), "application/x-www-form-urlencoded"),
+
     Params=[{"status", Status}],
 
 
     io:format("~p~n", [Headers]),
 
     httpc:request(post,
-		  {"https://mastodon.hccp.org/api/v1/statuses",
+		  {string:concat(Url, "/api/v1/statuses"),
 		   Headers,
 		   "application/x-www-form-urlencoded",
 		   Body
 		  }, [], []).
 
 
-
-
-
-
-%    MultiPartBody=generate_multipart__body(Params, "------boundary-----"),
-%    {ok, Response} = httpc:request(post,
-%				   {Url,
-%				    Headers,
-%				    "multipart/form-data",
-%				    MultiPartBody
-%				   }, [], [{headers_as_is, true},{body_format, binary}]).
-
-
 toot(Status, Images, AuthToken, Url)->
-    ok.
+    MediaIds=upload_images(Images, AuthToken, Url). 
+    
+upload_images(Images, AuthToken, Url)->
+    upload_images(Images, AuthToken, Url, []).
+    
+
+upload_images([H|T], AuthToken, Url, Acc)->
+    MediaId=upload_image(H, AuthToken, Url),
+    NewAcc=lists:append(Acc, [MediaId]),
+    upload_images(T, AuthToken, Url, NewAcc);
+upload_images([], AuthToken, Url, Acc)->
+    Acc.
+
+upload_image(Image, AuthToken, Url)->
+
+    
+    {ok, Media} = file:read_file(Image),
+
+    Params = [{"file", base64:decode_to_string(Media)}],
+    
+    MultiPartBody=generate_multipart__body(Params, "------boundary-----"),
+
+    Headers = get_headers(AuthToken, length(MultiPartBody), "multipart/form-data"),
+    {ok, Response} = httpc:request(post,
+				   {Url,
+				    Headers,
+				    "multipart/form-data",
+				    MultiPartBody
+				   }, [], [{headers_as_is, true},{body_format, binary}]).
+
+
+
 
 generate_multipart__body(Params, Boundary)->
     generate_multipart__body(Params, Boundary, []).
